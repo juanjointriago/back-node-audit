2733bdc11251a6c3d08da43a147bada188dabc92
Luis Lainez <luis.lainez@conauto.com.ec>
2025-01-08 09:18:50 -0500

    Add 'has_audited' in query for getEntities

--- controller/audit.ts
+++ controller/audit.ts
@@ -41,7 +41,19 @@ export const inactiveTrigger = async(req: Request, res: Response) => {
 
 export const getEntities = async(req: Request, res: Response) => {
     try {
-        const response = await prisma.$queryRaw`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' and table_name not like '%migrations%' and table_name not like 'Log';`;
+        const response = await prisma.$queryRaw`
+        SELECT
+                t.table_name,
+            MAX(CASE WHEN pt.tgname IS NOT NULL THEN 1 ELSE 0 END) AS has_audited
+        FROM information_schema.tables t
+        LEFT JOIN pg_trigger pt
+        ON pt.tgrelid = (quote_ident(t.table_name))::regclass and NOT pt.tgisinternal
+        WHERE
+            t.table_schema = 'public'
+            and t.table_type = 'BASE TABLE'
+            and t.table_name not like '%migrations%' and t.table_name not like 'Log'
+        GROUP BY t.table_name;
+        `;
         res.json({
             msg: 'ok',
             error: false,
